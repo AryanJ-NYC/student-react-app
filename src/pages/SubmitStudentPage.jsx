144a0a5aa83a5c6ceffb018043db97bbc770e047
Aryan Jabbari <AryanJabbari@gmail.com>
2024-11-13 12:32:03 -0500

    Add react-hook-form

--- src/pages/SubmitStudentPage.jsx
+++ src/pages/SubmitStudentPage.jsx
@@ -1,45 +1,31 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 export const SubmitStudentPage = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [major, setMajor] = useState('');
-  const [school, setSchool] = useState('');
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    await fetch(`${import.meta.env.VITE_API_URL}/students`, {
+      body: JSON.stringify({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        major: data.major,
+        school: data.school,
+      }),
+      headers: { 'content-type': 'application/json' },
+      method: 'POST',
+    });
+  };
 
   // 1. add onChange handlers to each input to update the state
   // 2. add onSubmit handler to the form to submit the data
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="First Name"
-        onChange={(event) => setFirstName(event.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        onChange={(event) => setLastName(event.target.value)}
-      />
-      <input type="text" placeholder="Major" onChange={(event) => setMajor(event.target.value)} />
-      <input type="text" placeholder="School" onChange={(event) => setSchool(event.target.value)} />
-      <button
-        onClick={async () => {
-          await fetch(`${import.meta.env.VITE_API_URL}/students`, {
-            body: JSON.stringify({
-              firstName: firstName,
-              lastName: lastName,
-              major: major,
-              school: school,
-            }),
-            headers: { 'content-type': 'application/json' },
-            method: 'POST',
-          });
-        }}
-        type="button"
-      >
-        Submit
-      </button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First Name" {...register('firstName')} />
+      <input type="text" placeholder="Last Name" {...register('lastName')} />
+      <input type="text" placeholder="Major" {...register('major')} />
+      <input type="text" placeholder="School" {...register('school')} />
+      <button type="submit">Submit</button>
     </form>
   );
 };
